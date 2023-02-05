18c33c9eac3cc3927a44d37a4a9543553fb85111
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-02-05 18:53:32 +0330

    add setterGenerator Function without using this keyword

--- Question1-returnFunc/app.js
+++ Question1-returnFunc/app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const user = {};
+
+function setterGenerator(key) {
+  return (value) => {
+    user[key] = value;
+    return user;
+  };
+}
+const nameSetter = setterGenerator("name");
+nameSetter("jack");
+console.log(user); // {name: ‘jack’}
