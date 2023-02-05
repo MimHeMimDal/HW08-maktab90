d26fdd0f4a2d8d318a3b71533d07bb3cb636637f
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-02-05 19:38:23 +0330

    crete counterMaker function

--- Question3-closure/app.js
+++ Question3-closure/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function counterMaker() {
+  let counter = 0;
+  return function (num) {
+    return num ? (counter += num) : counter;
+  };
+}
+
+const counter = counterMaker();
+
+console.log(counter());
+console.log(counter(1));
+console.log(counter(2));
+console.log(counter(-5));
+console.log(counter());
+console.log(counter());
