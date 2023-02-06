13130898bf10bc96a22754631049676cb7bdb1e6
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-02-06 17:30:47 +0330

    add a function to use this keyword

--- Question1-returnFunc/app.js
+++ Question1-returnFunc/app.js
@@ -2,9 +2,27 @@
 
 const user = {};
 
+/////////////////// edit user without this
+
+// function setterGenerator(key) {
+//   return (value) => {
+//     user[key] = value;
+//     return user;
+//   };
+// }
+// const nameSetter = setterGenerator("name");
+// nameSetter("jack");
+// console.log(user); // {name: ‘jack’}
+
+////////////// edit user with this
+
+function useThis([key, value]) {
+  this[key] = value;
+}
+
 function setterGenerator(key) {
   return (value) => {
-    user[key] = value;
+    useThis.call(user, [key, value]);
     return user;
   };
 }
